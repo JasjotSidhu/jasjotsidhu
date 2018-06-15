7822dbdcf9a41aee1398bf3bd1b88b864d97d977
Jasjot <jasjot.singh30@gmail.com>
2018-06-15 17:00:15 +0530

    code refactoring commit 2

--- src/client/containers/app/index.js
+++ src/client/containers/app/index.js
@@ -1,22 +1,3 @@
-import React, { Component } from 'react'
-import logo from '../../images/logo.svg'
-import './style.css'
-import { Button } from 'antd'
+import Component from './main';
 
-class App extends Component {
-    render () {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome Jass Sidhu</h1>
-                </header>
-                <p className="App-intro">
-                    <Button type="primary">Primary</Button>
-                </p>
-            </div>
-        )
-    }
-}
-
-export default App
+export default Component;
---
+++ src/client/containers/app/view.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../../images/logo.svg'
+import { Button } from 'antd'
+import './style.scss'
+
+var view = function () {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Welcome Jass Sidhu</h1>
+            </header>
+            <p className="App-intro">
+                <Button type="primary">Primary</Button>
+            </p>
+        </div>
+    )
+}
+
+export default view
