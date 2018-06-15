5769e7f40ee16fa6e207f1f3f358578a2154614a
Jasjot <jasjot.singh30@gmail.com>
2018-06-15 21:43:50 +0530

    new containers

---
+++ src/client/containers/home/view.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {} from 'antd'
+import './style.css'
+
+var view = function () {
+    return (
+        <div className="sample">
+
+        </div>
+    )
+}
+
+export default view
