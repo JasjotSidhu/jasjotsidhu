6d5c9e396e6c705b6e62a552027e7d4d05f1b821
Jasjot <jasjot.singh30@gmail.com>
2018-06-25 02:39:39 +0530

    paintings

--- src/client/containers/paintings/view.jsx
+++ src/client/containers/paintings/view.jsx
@@ -3,17 +3,30 @@ import {} from 'antd'
 import './style.css'
 import Gallery from 'react-photo-gallery'
 import LightBox from 'react-images'
+import africa from '../../images/art/africa.jpg'
+import buddha from '../../images/art/buddha.jpg'
+import girl from '../../images/art/girl.jpeg'
+import ballons from '../../images/art/balloons.jpg'
+import girl2 from '../../images/art/girl2.jpg'
+import horse from '../../images/art/horse.jpg'
+import lotus from '../../images/art/lotus.jpg'
+import rajdarbar from '../../images/art/rajdarbar.png'
+import tryion from '../../images/art/tryion.jpg'
+import tintin from '../../images/art/tintin.jpg'
+import wolf from '../../images/art/wolf.jpg'
 
 const photos = [
-    {src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3},
-    {src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1},
-    {src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4},
-    {src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4},
-    {src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4},
-    {src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3},
-    {src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4},
-    {src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3},
-    {src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3}
+    {src: africa, width: 4, height: 3},
+    {src: buddha, width: 4, height: 3},
+    {src: girl, width: 4, height: 3},
+    {src: ballons, width: 3, height: 4},
+    {src: girl2, width: 3, height: 4},
+    {src: rajdarbar, width: 3, height: 3},
+    {src: lotus, width: 3, height: 4},
+    {src: tryion, width: 4, height: 5},
+    {src: horse, width: 4.5, height: 5},
+    {src: tintin, width: 4, height: 3},
+    {src: wolf, width: 4, height: 3}
 ]
 
 let view = function () {
